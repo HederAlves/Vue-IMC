e94ef7a1f1428f7d1a547adc96867ebd42f76cfd
Heder Alves <alvesheder92@gmail.com>
2022-01-19 23:04:04 -0300

    Upload
    New files Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
-
-<template>
-  <div>
-    <img class="flex mx-auto" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" txt="teste"/>
-  </div>
-</template>
-
-<script setup lang="ts">
+<script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
 
 <style>
 #app {
