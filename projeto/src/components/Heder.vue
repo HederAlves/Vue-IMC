e283a1e9d662101694be2b541ffbb7552521da61
Heder Alves <alvesheder92@gmail.com>
2022-01-22 23:23:31 -0300

    Upload
    New files

---
+++ projeto/src/components/Heder.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Heder">
+        <h1>oi</h1>
+       <h1>{{ msg }}</h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Heder',
+    props: {
+        msg: String
+    }
+    
+}
+</script>
