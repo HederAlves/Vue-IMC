66f0b96d59f36cde699ae5abc71656d58b9d19d6
Heder Alves <alvesheder92@gmail.com>
2022-01-25 14:03:05 -0300

    Create/Update
    Adding calculation logic.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ titulo }}</h1>
+    <Header/>
+    <Tabela/>
+    <SectionUser/>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import Tabela from './components/Tabela.vue'
+import SectionUser from './components/SectionUser.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tabela,
+    SectionUser,
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    margin: 0 auto;
+    background: hsl(255,60%,60%);
+    padding: 60px 0px 0px 0px;
+    height: 607px;
+}
+</style>
